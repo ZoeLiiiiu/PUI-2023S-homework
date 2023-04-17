d958b362e9f8b1598d42cde4319a7b133b2a2f7f
ZoeLiiiiu <shiyueli@andrew.cmu.edu>
2023-04-17 13:00:42 -0400

    start project detail page

--- FP/js/javascript.js
+++ FP/js/javascript.js
@@ -1,3 +1,33 @@
+// should build a class that contain all the projects, the element properties are the project category and project alt text
+const projects = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "strawberry-cinnamon-roll.jpg"
+    }    
+};
+
+
+
 //Utilize the URL Parameters to generate different pages base on user's product selection
 //URL Parameters starts here
 
@@ -13,14 +43,15 @@ if (headerElement != null){
     headerElement.innerText = chosenProduct
 }
 
-/*
+
 // Update the image
-const productImage = document.querySelector('#image-on-project-detail-page');
+const productImage = document.querySelector('#project-detail-image');
 if (productImage != null){
-      productImage.src = 'asset/art/' + chosenProduct.imageFile;
-      productImage.alt = chosenProduct + ' Cinnamon Roll'
+      productImage.src = 'asset/' + chosenProduct + '.jpg';
+      productImage.alt = chosenProduct
   }
 
+  /*
 // Update the text
 const productPrice = document.querySelector('#total-price');
 if (productPrice != null){
